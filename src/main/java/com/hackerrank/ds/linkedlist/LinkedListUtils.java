01d9eea406865645c5a41810a5807cb4116a3bdb
suryasnat_das <suryasnat_das@apple.com>
2017-04-30 01:33:39 -0700

    hr

---
+++ src/main/java/com/hackerrank/ds/linkedlist/LinkedListUtils.java
@@ -0,0 +1,99 @@
+package com.hackerrank.ds.linkedlist;
+
+import com.algo.ds.linkedlist.Link;
+
+public class LinkedListUtils {
+	
+	public int compareLists(Link a, Link b){
+		while(a != null && b != null){
+			if(a.getKey() != b.getKey()) return 0;
+			a = a.next;
+			b = b.next;
+		}
+		return a == null && b == null ? 1 : 0;
+	}
+	
+	public Link mergeSortedListsRecursive(Link a, Link b){
+		if(a == null) return b;
+		if(b == null) return a;
+		if(a.getKey() < b.getKey()){
+			a.next = mergeSortedListsRecursive(a.next, b);
+			return a;
+		}else{
+			b.next = mergeSortedListsRecursive(a, b.next);
+			return b;
+		}
+	}
+	
+	public Link mergeSortedListsNonRecursive(Link a, Link b){
+		Link temp = null;
+		while(a != null && b != null){
+			if(a.getKey() < b.getKey()){
+				temp = formSortedList(a, temp);
+				a = a.next;
+			}else{
+				temp = formSortedList(b, temp);
+				b = b.next;
+			}
+		}
+		while(a != null){
+			temp = formSortedList(a, temp);
+			a = a.next;
+		}
+		while(b != null){
+			temp = formSortedList(b, temp);
+			b = b.next;
+		}
+		return temp;
+	}
+
+	private Link formSortedList(Link tempLink, Link head) {
+		Link current = head;
+		Link temp = new Link(tempLink.getKey());
+		if(head == null){
+			head = temp;
+		}else{
+			current.next = temp;
+			current = current.next;
+		}
+		return head;
+	}
+	
+	private void displayList(Link head){
+		while(head != null){
+			System.out.println(head.getKey());
+			head = head.next;
+		}
+	}
+	
+	public static void main(String[] args){
+		LinkedListUtils utils = new LinkedListUtils();
+		Link a = null;
+		Link b = null;
+//		System.out.println(utils.compareLists(a, b));
+		b = new Link(2);
+//		System.out.println(utils.compareLists(a, b));
+		a = new Link(3);
+		b = new Link(3);
+		a.next = b;
+//		System.out.println(utils.compareLists(a, b));
+		
+		Link a1 = new Link(1);
+		Link a2 = new Link(3);
+		Link a3 = new Link(5);
+		a1.next = a2;
+		a2.next = a3;
+		utils.displayList(a1);
+		System.out.println("********");
+		Link b1 = new Link(3);
+		Link b2 = new Link(4);
+		b1.next = b2;
+		utils.displayList(b1);
+//		a.next = new Link(5);
+		System.out.println("********");
+		utils.displayList(utils.mergeSortedListsRecursive(a1, b1));
+		System.out.println("********");
+		utils.displayList(utils.mergeSortedListsNonRecursive(a1, b1));
+	}
+
+}
