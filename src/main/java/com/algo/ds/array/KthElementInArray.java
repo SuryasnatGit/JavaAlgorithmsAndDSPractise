ddee56160f9929bde3de61f3a2d1757289c34242
Suryasnat Das <surya.das@gmail.com>
2017-12-11 02:01:07 -0800

    more code added

--- src/main/java/com/algo/ds/array/KthElementInArray.java
+++ src/main/java/com/algo/ds/array/KthElementInArray.java
@@ -1,65 +1,112 @@
-package com.interview.array;
+package com.algo.ds.array;
 
 import java.util.Arrays;
 
+import com.algo.ds.graph.BinaryMaxHeap;
+import com.algo.ds.graph.BinaryMinHeap;
+import com.algo.ds.queue.PriorityQueueAsArray;
+
 /**
- * Kth largest element in an array.
- * Use quickselect of quicksort to find the solution in hopefully O(nlogn) time.
- * Test cases
- * Sorted array
- * Reverse sorted array
+ * Kth largest element in an array. Use quick select of quicksort to find the solution in hopefully O(nlogn) time. Test
+ * cases Sorted array Reverse sorted array
  */
 public class KthElementInArray {
-    
-    public int kthElement(int arr[],int k){
-        int low = 0;
-        int high = arr.length-1;
-        int pos =0;
-        while(true){
-            pos = quickSelect(arr,low,high);
-            if(pos == (k+low)){
-                return arr[pos];
-            }
-            if(pos > k + low){
-                high = pos-1;
-            }else{
-                k = k - (pos - low + 1);
-                low = pos+1;
-            }
-        }
-    }
-    
-    private int quickSelect(int arr[],int low,int high){
-        int pivot = low;
-        low++;
-        while(low <= high){
-            if(arr[pivot] > arr[low]){
-                low++;
-                continue;
-            }
-            if(arr[pivot] <= arr[high]){
-                high--;
-                continue;
-            }
-            swap(arr,low,high);
-        }
-        if(arr[high] < arr[pivot]){
-            swap(arr,pivot,high);
-        }
-        return high;
-    }
-    
-    private void swap(int arr[],int low,int high){
-        int temp = arr[low];
-        arr[low] = arr[high];
-        arr[high] = temp;
-    }
-    
-    public static void main(String args[]){
-        int arr[] = {6, 2, 1, 6, 8, 9, 6};
-        KthElementInArray kthElement = new KthElementInArray();
-        System.out.print(kthElement.kthElement(arr, arr.length/2));
-        System.out.print(Arrays.toString(arr));
-    }
-    
+
+	/**
+	 * Time - O(nlogn)
+	 * 
+	 * @param arr
+	 * @param k
+	 * @return
+	 */
+	public int kthLargestSimple(int[] arr, int k) {
+		// sort array
+		Arrays.sort(arr);
+		return arr[arr.length - k - 1];
+	}
+
+	public int kthLargestUsingPriorityQueue(int[] arr, int k) {
+		PriorityQueueAsArray queue = new PriorityQueueAsArray(arr.length);
+	}
+
+	public int kthLargestUsingMinHeap(int[] arr, int k) {
+		BinaryMinHeap<Integer> heap = new BinaryMinHeap<>();
+		for (int i = 0; i < arr.length; i++) {
+			heap.add(arr[i], arr[i]);
+		}
+		int num = heap.extractMin();
+	}
+
+	/**
+	 * 1) Build a Max Heap tree in O(n) 2) Use Extract Max k times to get k maximum elements from the Max Heap O(klogn)
+	 * 
+	 * Time complexity: O(n + klogn)
+	 * 
+	 * @param arr
+	 * @param k
+	 * @return
+	 */
+	public int kthLargestUsingMaxHeap(int[] arr, int k) {
+		BinaryMaxHeap<Integer> heap = new BinaryMaxHeap<>();
+		for (int i = 0; i < arr.length; i++) {
+			heap.add(arr[i], arr[i]);
+		}
+		int res = 0;
+		for (int i = 0; i < k; i++) {
+			res = heap.max();
+		}
+		return res;
+	}
+
+	public int kthElement(int arr[], int k) {
+		int low = 0;
+		int high = arr.length - 1;
+		int pos = 0;
+		while (true) {
+			pos = quickSelect(arr, low, high);
+			if (pos == (k + low)) {
+				return arr[pos];
+			}
+			if (pos > k + low) {
+				high = pos - 1;
+			} else {
+				k = k - (pos - low + 1);
+				low = pos + 1;
+			}
+		}
+	}
+
+	private int quickSelect(int arr[], int low, int high) {
+		int pivot = low;
+		low++;
+		while (low <= high) {
+			if (arr[pivot] > arr[low]) {
+				low++;
+				continue;
+			}
+			if (arr[pivot] <= arr[high]) {
+				high--;
+				continue;
+			}
+			swap(arr, low, high);
+		}
+		if (arr[high] < arr[pivot]) {
+			swap(arr, pivot, high);
+		}
+		return high;
+	}
+
+	private void swap(int arr[], int low, int high) {
+		int temp = arr[low];
+		arr[low] = arr[high];
+		arr[high] = temp;
+	}
+
+	public static void main(String args[]) {
+		int arr[] = { 6, 2, 1, 6, 8, 9, 6 };
+		KthElementInArray kthElement = new KthElementInArray();
+		System.out.print(kthElement.kthElement(arr, arr.length / 2));
+		System.out.print(Arrays.toString(arr));
+	}
+
 }
--- src/main/java/com/algo/string/string/CycleLeaderIteration.java
+++ src/main/java/com/algo/string/string/CycleLeaderIteration.java
@@ -1,39 +1,52 @@
-package com.interview.string;
+package com.algo.string.string;
 
+/**
+ * a[i] = a[a[i]] in-place Write a program to modify the array such that arr[I] = arr[arr[I]]. Do this in place i.e.
+ * with out using additional memory.
+ * 
+ * input : {2,3,1,0} output : {1,0,3,2}
+ * 
+ * This is a simple application of cycle leader iteration algorithm.
+ * 
+ * Things to note : size of input array will always be of the form 3^k+1 ie,2, 4, 10, 28, 82..
+ * 
+ * @author Suryasnat
+ *
+ */
 public class CycleLeaderIteration {
 
-    //assumption that size is going to be 3^k +1 from start to end
-    public void iterate(char str[],int start,int end){
-        int len = end - start +1;
-        int power = 1;
-        while(power < len){
-            int index = power;
-            int newIndex = -1;
-            char temp = str[start+index];
-            char temp1;
-            while(newIndex != power){
-                if(index % 2 ==0){
-                    newIndex = index/2;
-                }else{
-                    newIndex = len/2 + index/2;
-                }
-                temp1 = str[start + newIndex];
-                str[start+newIndex] = temp;
-                temp = temp1;
-                index = newIndex;
-            }
-            power = power*3;        
-        }
-    }
-    
-    public static void main(String args[]){
-        String str = "1a2b3c4d5e6f7g8h9iAjBkClDmEn";
-        char[] str1 = str.toCharArray();
-        CycleLeaderIteration cli = new CycleLeaderIteration();
-        cli.iterate(str1, 0, str1.length);
-        for(char ch: str1){
-            System.out.print(ch + " ");
-        }
-    }
-    
+	// assumption that size is going to be 3^k +1 from start to end
+	public void iterate(char str[], int start, int end) {
+		int len = end - start + 1;
+		int power = 1;
+		while (power < len) {
+			int index = power;
+			int newIndex = -1;
+			char temp = str[start + index];
+			char temp1;
+			while (newIndex != power) {
+				if (index % 2 == 0) {
+					newIndex = index / 2;
+				} else {
+					newIndex = len / 2 + index / 2;
+				}
+				temp1 = str[start + newIndex];
+				str[start + newIndex] = temp;
+				temp = temp1;
+				index = newIndex;
+			}
+			power = power * 3;
+		}
+	}
+
+	public static void main(String args[]) {
+		String str = "1a2b3c4d5e6f7g8h9iAjBkClDmEn";
+		char[] str1 = str.toCharArray();
+		CycleLeaderIteration cli = new CycleLeaderIteration();
+		cli.iterate(str1, 0, str1.length);
+		for (char ch : str1) {
+			System.out.print(ch + " ");
+		}
+	}
+
 }
