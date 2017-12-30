5c7a95572348e589759a6477ec116609411d4296
Suryasnat Das <surya.das@gmail.com>
2017-12-30 01:22:29 -0800

    ll code update

--- src/main/java/com/algo/ds/linkedlist/FlattenLinkList.java
+++ src/main/java/com/algo/ds/linkedlist/FlattenLinkList.java
@@ -1,78 +1,117 @@
-package com.interview.linklist;
+package com.algo.ds.linkedlist;
 
 /**
  * http://www.geeksforgeeks.org/flatten-a-linked-list-with-next-and-child-pointers/
- * Test case
- * 0 node in the list
- * 1 node in the list
- * All nodes with child
+ * 
+ * Test case:<br/>
+ * 0 node in the list <br/>
+ * 1 node in the list <br/>
+ * All nodes with child <br/>
  * No nodes with child
  */
 public class FlattenLinkList {
 
-    public void flatten(Node head) {
-        Node tail = getTail(head);
-        while (head != null) {
-            if (head.child != null) {
-                tail.next = head.child;
-                tail = getTail(tail.next);
-                head.child = null;
-            }
-            head = head.next;
-        }
-    }
+	/**
+	 * Time Complexity: Since every node is visited at most twice, the time complexity is O(n) where n is the number of
+	 * nodes in given linked list.
+	 * 
+	 * @param head
+	 */
+	public void flattenList_linear(Node head) {
+		if (head == null)
+			return;
 
-    private Node getTail(Node head) {
-        if (head == null) {
-            return null;
-        }
-        while (head.next != null) {
-            head = head.next;
-        }
-        return head;
-    }
+		Node tmp = null;
 
-    public static void main(String args[]) {
-        LinkList ll = new LinkList();
-        Node head = null;
-        head = ll.addNode(10, head);
-        head = ll.addNode(5, head);
-        head = ll.addNode(12, head);
-        head = ll.addNode(7, head);
-        head = ll.addNode(11, head);
+		// find tail node of current level
+		Node tail = head;
+		while (tail.next != null)
+			tail = tail.next;
 
-        Node head1 = null;
-        head1 = ll.addNode(4, head1);
-        head1 = ll.addNode(20, head1);
-        head1 = ll.addNode(13, head1);
+		// one by one traverse through all nodes of list
+		Node cur = head;
+		while (cur != tail) {
+			// if(cur has child
+			if (cur.child != null) {
+				// append child at end of current list
+				tail.next = cur.child;
 
-        Node head2 = null;
-        head2 = ll.addNode(2, head2);
-        head2 = ll.addNode(8, head2);
+				// navigate to end of cur child
+				tmp = cur.child;
+				while (tmp.next != null)
+					tmp = tmp.next;
+				// update tail to new last node
+				tail = tmp;
+			}
 
-        Node head4 = null;
-        head4 = ll.addNode(17, head4);
-        head4 = ll.addNode(6, head4);
+			cur = cur.next;
+		}
 
-        Node head5 = null;
-        head5 = ll.addNode(9, head5);
-        head5 = ll.addNode(8, head5);
-        head5 = ll.addNode(15, head5);
+	}
 
-        Node f1 = ll.find(head, 10);
-        f1.child = head1;
+	public void flatten_recursive(Node head) {
+		Node tail = getTail(head);
+		while (head != null) {
+			if (head.child != null) {
+				tail.next = head.child;
+				tail = getTail(tail.next);
+				head.child = null;
+			}
+			head = head.next;
+		}
+	}
 
-        f1 = ll.find(head, 7);
-        f1.child = head4;
+	private Node getTail(Node head) {
+		if (head == null) {
+			return null;
+		}
+		while (head.next != null) {
+			head = head.next;
+		}
+		return head;
+	}
 
-        f1 = ll.find(head4, 17);
-        f1.child = head5;
+	public static void main(String args[]) {
+		LinkList ll = new LinkList();
+		Node head = null;
+		head = ll.addNode(10, head);
+		head = ll.addNode(5, head);
+		head = ll.addNode(12, head);
+		head = ll.addNode(7, head);
+		head = ll.addNode(11, head);
 
-        f1 = ll.find(head1, 20);
-        f1.child = head2;
-        
-        FlattenLinkList fll = new FlattenLinkList();
-        fll.flatten(head);
-        ll.printList(head);
-    }
+		Node head1 = null;
+		head1 = ll.addNode(4, head1);
+		head1 = ll.addNode(20, head1);
+		head1 = ll.addNode(13, head1);
+
+		Node head2 = null;
+		head2 = ll.addNode(2, head2);
+		head2 = ll.addNode(8, head2);
+
+		Node head4 = null;
+		head4 = ll.addNode(17, head4);
+		head4 = ll.addNode(6, head4);
+
+		Node head5 = null;
+		head5 = ll.addNode(9, head5);
+		head5 = ll.addNode(8, head5);
+		head5 = ll.addNode(15, head5);
+
+		Node f1 = ll.find(head, 10);
+		f1.child = head1;
+
+		f1 = ll.find(head, 7);
+		f1.child = head4;
+
+		f1 = ll.find(head4, 17);
+		f1.child = head5;
+
+		f1 = ll.find(head1, 20);
+		f1.child = head2;
+
+		FlattenLinkList fll = new FlattenLinkList();
+		fll.flatten_recursive(head);
+		ll.printList(head);
+	}
 }
--- src/main/java/com/algo/ds/linkedlist/LinkListIsPalindrome.java
+++ src/main/java/com/algo/ds/linkedlist/LinkListIsPalindrome.java
@@ -1,39 +1,160 @@
-package com.interview.linklist;
+package com.algo.ds.linkedlist;
+
+import com.algo.ds.stack.LinkedListStack;
+import com.algo.ds.stack.Stack;
 
 /**
- * http://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/ 
- * Test cases:
- * odd number of nodes
- * even number of nodes
- * 0 1 or more nodes
- * palindrome list
+ * http://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/ Test cases: <br/>
+ * odd number of nodes <br/>
+ * even number of nodes <br/>
+ * 0 1 or more nodes <br/>
+ * palindrome list <br/>
  * non palindrom list
  */
 public class LinkListIsPalindrome {
 
-    public boolean isPalindrome(NodeRef head,Node end){
-        if(end == null){
-            return true;
-        }
-        boolean r = isPalindrome(head,end.next);
-        r = r && head.node.data == end.data;
-        head.next();
-        return r;
-    }
-    
-    public static void main(String args[]){
-        LinkList ll = new LinkList();
-        Node head = null;
-        head = ll.addNode(1, head);
-        head = ll.addNode(2, head);
-        head = ll.addNode(3, head);
-        head = ll.addNode(4, head);
-        head = ll.addNode(3, head);
-        head = ll.addNode(2, head);
-        head = ll.addNode(1, head);
-        NodeRef nodeRef = new NodeRef();
-        nodeRef.node = head;
-        LinkListIsPalindrome llp = new LinkListIsPalindrome();
-        System.out.println(llp.isPalindrome(nodeRef, head));
-    }
+	/**
+	 * Time complexity of above method is O(n), but it requires O(n) extra space.
+	 * 
+	 * @param headRef
+	 * @param head
+	 * @return
+	 */
+	public boolean isPalindrome_usingStack(NodeRef headRef, Node head) {
+		Stack<Integer> stack = new LinkedListStack<>();
+		Node temp = head;
+		// 1st pass
+		while (temp != null) {
+			stack.push(temp.data);
+			temp = temp.next;
+		}
+		// 2nd pass
+		temp = head;
+		while (temp != null) {
+			if (!stack.pop().equals(temp.data))
+				return false;
+			temp = temp.next;
+		}
+		return true;
+	}
+
+	// declare pointers to head, slow pointer, fast pointer and second half of list for using this method
+	Node slow, fast, secondHalf;
+
+	/**
+	 * Time Complexity O(n) Auxiliary Space: O(1)
+	 * 
+	 * @param head
+	 * @return
+	 */
+	public boolean isPalindrome_usingReverseMethod(Node head) {
+		// initially slow and fast point to head
+		slow = head;
+		fast = head;
+		Node prevToSlow = head; // points to a node previous to slow pointer
+		Node midNode = null; // pointer for middle node(this will be null for even number of nodes and not null for odd
+								// number of nodes)
+		boolean result = true;
+		if (head != null && head.next != null) {
+			// move slow and fast pointers
+			while (fast != null && fast.next != null) {
+				fast = fast.next.next;
+				// move prevToSlow
+				prevToSlow = slow;
+				slow = slow.next;
+			}
+
+			// for odd number of nodes fast will be not null. for even number of nodes fast will be null.
+			// skip the middle node for odd case and store it so that it can be used later for restoring original list
+			if (fast != null) {
+				midNode = slow;
+				slow = slow.next;
+			}
+
+			// now split the lists into 2
+			secondHalf = slow;
+			// terminate the first half
+			prevToSlow.next = null;
+			// reverse second half
+			reverse();
+			// compare first and second halfs
+			result = compare(head, secondHalf);
+			// restore the original list by again reversing the second half
+			reverse();
+
+			// check for midnode nullability
+			if (midNode != null) {
+				prevToSlow.next = midNode;
+				midNode.next = secondHalf;
+			} else
+				prevToSlow.next = secondHalf;
+		}
+		return result;
+	}
+
+	private boolean compare(Node head1, Node head2) {
+		Node temp1 = head1;
+		Node temp2 = head2;
+
+		while (temp1 != null && temp2 != null) {
+			if (temp1.data == temp2.data) {
+				temp1 = temp1.next;
+				temp2 = temp2.next;
+			} else
+				return false;
+		}
+		if (temp1 == null && temp2 == null)
+			return true;
+
+		// will reach here if one is null and other is not null
+		return false;
+	}
+
+	private void reverse() {
+		Node prev = null;
+		Node current = secondHalf;
+		Node next;
+		while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		secondHalf = prev;
+	}
+
+	/**
+	 * Time Complexity: O(n) Auxiliary Space: O(n) if Function Call Stack size is considered, otherwise O(1).
+	 * 
+	 * @param head
+	 * @param end
+	 * @return
+	 */
+	public boolean isPalindrome(NodeRef head, Node end) {
+		if (end == null) {
+			return true;
+		}
+		boolean r = isPalindrome(head, end.next);
+		r = r && head.node.data == end.data;
+		head.next();
+		return r;
+	}
+
+	public static void main(String args[]) {
+		LinkList ll = new LinkList();
+		Node head = null;
+		head = ll.addNode(1, head);
+		head = ll.addNode(2, head);
+		head = ll.addNode(3, head);
+		head = ll.addNode(4, head);
+		head = ll.addNode(3, head);
+		head = ll.addNode(2, head);
+		head = ll.addNode(1, head);
+		NodeRef nodeRef = new NodeRef();
+		nodeRef.node = head;
+		LinkListIsPalindrome llp = new LinkListIsPalindrome();
+		System.out.println(llp.isPalindrome(nodeRef, head));
+		System.out.println(llp.isPalindrome_usingStack(nodeRef, head));
+		System.out.println(llp.isPalindrome_usingReverseMethod(head));
+	}
 }
