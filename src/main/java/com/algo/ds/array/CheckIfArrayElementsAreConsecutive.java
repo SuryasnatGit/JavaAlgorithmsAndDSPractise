b7670d389ab23648f31463048dae4153aec9129f
Suryasnat Das <surya.das@gmail.com>
2018-01-04 09:42:10 -0800

    local

--- src/main/java/com/algo/ds/array/CheckIfArrayElementsAreConsecutive.java
+++ src/main/java/com/algo/ds/array/CheckIfArrayElementsAreConsecutive.java
@@ -1,35 +1,125 @@
-package com.interview.array;
+package com.algo.ds.array;
+
+import java.util.Arrays;
 
 /**
  * http://www.geeksforgeeks.org/check-if-array-elements-are-consecutive/
+ * 
+ * Given an unsorted array of numbers, write a function that returns true if array consists of consecutive numbers.
+ * 
+ * Examples: a) If array is {5, 2, 3, 1, 4}, then the function should return true because the array has consecutive
+ * numbers from 1 to 5.
+ * 
+ * b) If array is {83, 78, 80, 81, 79, 82}, then the function should return true because the array has consecutive
+ * numbers from 78 to 83.
+ * 
+ * c) If the array is {34, 23, 52, 12, 3 }, then the function should return false because the elements are not
+ * consecutive.
+ * 
+ * d) If the array is {7, 6, 5, 5, 3, 4}, then the function should return false because 5 and 5 are not consecutive.
+ * 
+ * 
  */
 public class CheckIfArrayElementsAreConsecutive {
 
-    public boolean areConsecutive(int input[]){
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i < input.length; i++){
-            if(input[i] < min){
-                min = input[i];
-            }
-        }
-        for(int i=0; i < input.length; i++){
-            if(Math.abs(input[i]) - min >= input.length){
-                return false;
-            }
-            if(input[Math.abs(input[i]) - min] < 0){
-                return false;
-            }
-            input[Math.abs(input[i]) - min] = -input[Math.abs(input[i]) - min];
-        }
-        for(int i=0; i < input.length ; i++){
-            input[i] = Math.abs(input[i]);
-        }
-        return true;
-    }
-    
-    public static void main(String args[]){
-        int input[] = {76,78,76,77,73,74};
-        CheckIfArrayElementsAreConsecutive cia = new CheckIfArrayElementsAreConsecutive();
-        System.out.println(cia.areConsecutive(input));
-    }
+	/**
+	 * Time Complexity: O(nLogn)
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public boolean areConsequetive_usingSorting(int[] input) {
+		Arrays.sort(input); // O(n log n)
+		for (int i = 0; i < input.length - 1; i++) {
+			if (input[i + 1] - input[i] != 1)
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Time Complexity: O(n) Extra Space: O(n)
+	 * 
+	 * @param input
+	 * @param n
+	 * @return
+	 */
+	public boolean areConsequetive_usingVisitedArray(int[] input, int n) {
+		if (n < 1)
+			return false;
+
+		int min = findMin(input, n);
+
+		int max = findMax(input, n);
+
+		if (max - min + 1 == n) { // then only check for all elements
+			// have a visited array of size n
+			boolean[] arr = new boolean[n];
+			for (int i = 0; i < n; i++) {
+				if (arr[input[i] - min] != false) // this means the element is already visited
+					return false;
+				arr[input[i] - min] = true;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private int findMin(int[] input, int n) {
+		int min = input[0];
+		for (int i = 1; i < n; i++) {
+			if (input[i] < min)
+				min = input[i];
+		}
+		return min;
+	}
+
+	private int findMax(int[] input, int n) {
+		int max = input[0];
+		for (int i = 1; i < n; i++) {
+			if (input[i] > max)
+				max = input[i];
+		}
+		return max;
+	}
+
+	/**
+	 * This method is O(n) time complexity and O(1) extra space, but it changes the original array and it works only if
+	 * all numbers are positive. We can get the original array by adding an extra step though. It is an extension of
+	 * method 2 and it has the same two steps.
+	 * 
+	 * Time Complexity: O(n) Extra Space: O(1)
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public boolean areConsecutive(int input[]) {
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] < min) {
+				min = input[i];
+			}
+		}
+		for (int i = 0; i < input.length; i++) {
+			if (Math.abs(input[i]) - min >= input.length) {
+				return false;
+			}
+			if (input[Math.abs(input[i]) - min] < 0) {
+				return false;
+			}
+			input[Math.abs(input[i]) - min] = -input[Math.abs(input[i]) - min];
+		}
+		for (int i = 0; i < input.length; i++) {
+			input[i] = Math.abs(input[i]);
+		}
+		return true;
+	}
+
+	public static void main(String args[]) {
+		int input[] = { 76, 78, 75, 77, 71, 74 };
+		CheckIfArrayElementsAreConsecutive cia = new CheckIfArrayElementsAreConsecutive();
+		System.out.println(cia.areConsecutive(input));
+		System.out.println(cia.areConsequetive_usingSorting(input));
+		System.out.println(cia.areConsequetive_usingVisitedArray(input, input.length));
+	}
 }
--- src/main/java/com/algo/ds/array/Flip0sMaximum1s.java
+++ src/main/java/com/algo/ds/array/Flip0sMaximum1s.java
@@ -1,48 +1,120 @@
-package com.interview.array;
+package com.algo.ds.array;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Date 12/29/2015
- * @author Tushar Roy
+ * Given input array of 0s and 1s and number of flips allowed from 0 to 1, what is maximum consecutive 1s we can have in
+ * array
  *
- * Given input array of 0s and 1s and number of flips allowed from 0 to 1, what is maximum consecutive 1s we can have
- * in array
- *
- * Time complexity - O(n)
- * Space complexity - O(1)
+ * Time complexity - O(n) Space complexity - O(1)
  *
  * http://www.geeksforgeeks.org/find-zeroes-to-be-flipped-so-that-number-of-consecutive-1s-is-maximized/
+ * 
+ * e.g. input: arr={1 1 0 1 1 0 0 1 1 1 } m=1 output={1 1 1 1 1 0 0 1 1 1} position=2
+ * 
+ * arr={1 1 0 1 1 0 0 1 1 1 } m=2 output={1 1 0 1 1 1 1 1 1 1} position=5,6
+ * 
+ * @author Tushar Roy
  */
 public class Flip0sMaximum1s {
 
-    public int flip0sToMaximizeConsecutive1s(int input[], int flipsAllowed) {
+	public int flip0sToMaximizeConsecutive1s(int input[], int flipsAllowed) {
 
-        int windowStart = 0;
-        int countZero = 0;
-        int result = 0;
-        for (int i = 0 ; i < input.length; i++) {
-            if (input[i] == 1) {
-                result = Math.max(result, i - windowStart + 1);
-            } else {
-                if (countZero < flipsAllowed) {
-                    countZero++;
-                    result = Math.max(result, i - windowStart + 1);
-                } else {
-                    while(true) {
-                        if (input[windowStart] == 0) {
-                            windowStart++;
-                            break;
-                        }
-                        windowStart++;
-                    }
-                }
-            }
-        }
-        return result;
-    }
+		int windowStart = 0;
+		int countZero = 0;
+		int result = 0;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] == 1) {
+				result = Math.max(result, i - windowStart + 1);
+			} else {
+				if (countZero < flipsAllowed) {
+					countZero++;
+					result = Math.max(result, i - windowStart + 1);
+				} else {
+					while (true) {
+						if (input[windowStart] == 0) {
+							windowStart++;
+							break;
+						}
+						windowStart++;
+					}
+				}
+			}
+		}
+		return result;
+	}
 
-    public static void main(String args[]) {
-        int input[] = {0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1};
-        Flip0sMaximum1s fm = new Flip0sMaximum1s();
-        System.out.print(fm.flip0sToMaximizeConsecutive1s(input, 1));
-    }
+	/**
+	 * You are given with an array of 1s and 0s. And you are given with an integer m, which signifies number of flips
+	 * allowed.
+	 * 
+	 * find the position of zeros which when flipped will produce maximum continuous series of 1s.
+	 * 
+	 * e.g. input: arr={1 1 0 1 1 0 0 1 1 1 } m=1 output={1 1 1 1 1 0 0 1 1 1} position=2
+	 * 
+	 * arr={1 1 0 1 1 0 0 1 1 1 } m=2 output={1 1 0 1 1 1 1 1 1 1} position=5,6
+	 * 
+	 * This is similar to the max subarray problem and can be solved in O(n) time using something similar to Kadane's
+	 * algorithm. The main idea is maintaining a sliding window that contains a continuous sequence of 1's as we iterate
+	 * over the array. We extend the right side of the window if we encounter a 1 or the number of flips is less than m;
+	 * otherwise we shrink the left side to one position to the right of the first zero. The last part forces us to
+	 * maintain a list of the positions of the zeros. Thus O(m) space. Here a solution in Java.
+	 * 
+	 * @param arr
+	 * @param m
+	 * @return
+	 */
+	public List<Integer> maximumContinuousOnes(int[] arr, int m) {
+		// initialize variables
+		int maxBegin = 0;
+		int maxEnd = 0;
+		int maxLength = 0;
+		int begin = 0;
+
+		// holder for 0 elements
+		ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				if (deque.size() < m) {
+					deque.addFirst(i);
+				} else {
+					int length = i - begin;
+					if (length > maxLength) {
+						maxLength = length;
+						maxBegin = begin;
+						maxEnd = i;
+					}
+					begin = deque.removeLast() + 1;
+					deque.addFirst(i);
+				}
+			}
+		}
+
+		int length = arr.length - begin;
+		if (length > maxLength) {
+			maxBegin = begin;
+			maxEnd = arr.length;
+		}
+
+		List<Integer> list = new ArrayList<>();
+		for (int i = maxBegin; i < maxEnd; i++) {
+			if (arr[i] == 0)
+				list.add(i);
+		}
+
+		return list;
+	}
+
+	public static void main(String args[]) {
+		int input[] = { 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1 };
+		Flip0sMaximum1s fm = new Flip0sMaximum1s();
+		System.out.println(fm.flip0sToMaximizeConsecutive1s(input, 1));
+
+		int[] input1 = { 1, 1, 0, 1, 1, 0, 0, 1, 1, 1 };
+		System.out.println(fm.maximumContinuousOnes(input1, 1));
+		System.out.println(fm.maximumContinuousOnes(input1, 2));
+	}
 }
