d592a96a8d3bcaab44ff0b1e8e3ca6bf79f6e995
surya <surya@DESKTOP-K3F19AG>
2018-05-24 01:59:47 -0700

    from windows

--- src/main/java/com/algo/ds/array/CountInversionOfSize3.java
+++ src/main/java/com/algo/ds/array/CountInversionOfSize3.java
@@ -1,29 +1,60 @@
-package com.interview.array;
+package com.algo.ds.array;
 
 /**
- * Date 12/29/15
- * @author Tushar Roy
- *
  * Given input array find number of inversions where i < j < k and input[i] > input[j] > input[k]
  *
- * http://www.geeksforgeeks.org/count-inversions-of-size-three-in-a-give-array/
+ * http://www.geeksforgeeks.org/count-inversions-of-size-three-in-a-give-array/.
+ * 
+ * Example:
+ * 
+ * Input: {8, 4, 2, 1} Output: 4 The four inversions are (8,4,2), (8,4,1), (4,2,1) and (8,2,1).
+ * 
+ * Input: {9, 6, 4, 5, 8} Output: 2 The two inversions are {9, 6, 4} and {9, 6, 5}
  */
 public class CountInversionOfSize3 {
 
+	/**
+	 * complexity = O(n^3)
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public int findInversions_Simple(int[] input) {
+		int inversion = 0;
+		int len = input.length;
+		for (int i = 0; i < len - 2; i++) {
+			for (int j = i + 1; j < len - 1; j++) {
+				if (input[i] > input[j]) {
+					for (int k = j + 1; k < len; k++) {
+						if (input[j] > input[k])
+							inversion++;
+					}
+				}
+			}
+		}
+		return inversion;
+	}
+
     /**
-     * Time complexity of this method is O(n^2)
-     * Space complexity is O(1)
-     */
+	 * We can reduce the complexity if we consider every element arr[i] as middle element of inversion,
+	 * find all the numbers greater than a[i] whose index is less than i, find all the numbers which are
+	 * smaller than a[i] and index is more than i. We multiply the number of elements greater than a[i]
+	 * to the number of elements smaller than a[i] and add it to the result. <br/>
+	 * Time complexity of this method is O(n^2)<br/>
+	 * Space complexity is O(1)
+	 */
     public int findInversions(int input[]) {
         int inversion = 0;
         for (int i = 1; i < input.length - 1 ; i++) {
             int larger = 0;
+			// count all large elements on left of input[i]
             for (int k = 0; k < i; k++) {
                 if (input[k] > input[i]) {
                     larger++;
                 }
             }
             int smaller = 0;
+			// count all small elements on right of input[i]
             for (int k = i+1; k < input.length; k++) {
                 if (input[k] < input[i]) {
                     smaller++;
@@ -33,6 +64,12 @@ public class CountInversionOfSize3 {
         }
         return inversion;
     }
+
+	/**
+	 * another approach is using Binary Indexed Tree which is covered in {@link CountInversionInArray}
+	 * 
+	 * @param args
+	 */
 
     public static void main(String args[]) {
         int input[] = {9, 6, 4, 5, 8};
--- src/main/java/com/algo/ds/array/KadaneWrapArray.java
+++ src/main/java/com/algo/ds/array/KadaneWrapArray.java
@@ -1,10 +1,16 @@
-package com.interview.array;
+package com.algo.ds.array;
 
 /**
- * http://www.geeksforgeeks.org/maximum-contiguous-circular-sum/
- * Test cases
- * All negative
- * All positives
+ * http://www.geeksforgeeks.org/maximum-contiguous-circular-sum/. The maximum subarray problem is
+ * the task of finding the contiguous subarray within a one-dimensional array of numbers which has
+ * the largest sum.
+ * 
+ * Example: int [] A = {−2, 1, −3, 4, −1, 2, 1, −5, 4}; Output: contiguous subarray with the largest
+ * sum is 4, −1, 2, 1, with sum 6.
+ * 
+ * Test cases:<br/>
+ * All negative<br/>
+ * All positives <br/>
  * all 0s
  */
 
@@ -17,10 +23,49 @@ class Triplet{
         return "Triplet [start=" + start + ", end=" + end + ", sum=" + sum
                 + "]";
     }
-    
 }
+
 public class KadaneWrapArray {
 
+	/**
+	 * complexity - O(n^3) in worst case.
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public int naiveSoln(int[] arr) {
+		int maxSum = 0;
+		// starting point
+		for (int i = 0; i < arr.length; i++) {
+			// ending point
+			for (int j = i; j < arr.length; j++) {
+				// subarray between current start and end points
+				int sum = 0;
+				for (int c = i; c <= j; c++) {
+					sum += arr[c];
+				}
+				if (sum > maxSum)
+					maxSum = sum;
+			}
+		}
+		return maxSum;
+	}
+
+	/**
+	 * complexity - O(n).
+	 * 
+	 * start: 
+	 * 	max_so_far = 0 
+	 * 	max_ending_here = 0
+	 * 
+	 * loop i= 0 to n 
+	 * 	(i) max_ending_here = max_ending_here + a[i] 
+	 * 	(ii) if(max_ending_here < 0) max_ending_here = 0 
+	 * 	(iii) if(max_so_far < max_ending_here) max_so_far = max_ending_here return max_so_far
+	 * 
+	 * @param arr
+	 * @return
+	 */
     public Triplet kadaneWrap(int arr[]){
         Triplet straightKadane = kadane(arr);
         int sum =0;
@@ -29,7 +74,7 @@ public class KadaneWrapArray {
             arr[i] = -arr[i];
         }
         Triplet wrappedNegKadane = kadane(arr);
-        for(int i=0; i < arr.length; i++){
+		for (int i = 0; i < arr.length; i++) {
             arr[i] = -arr[i];
         }
         if(straightKadane.sum < sum + wrappedNegKadane.sum){
@@ -72,11 +117,56 @@ public class KadaneWrapArray {
         return p;
     }
     
+	// Kadane algorithm
+	public int kandane(int[] arrA) {
+		int max_end_here = 0;
+		int max_so_far = 0;
+		for (int i = 0; i < arrA.length; i++) {
+			max_end_here += arrA[i];
+			if (max_end_here < 0) {
+				max_end_here = 0;
+			}
+			if (max_so_far < max_end_here) {
+				max_so_far = max_end_here;
+			}
+		}
+		return max_so_far;
+	}
+
+	// Below modification will allow the program to work even if all the
+	// elements in the array are negative
+	/**
+	 * start: 
+	 * 	max_so_far = a[0] 
+	 * 	max_ending_here = a[0]
+	 * 
+	 * loop i= 1 to n 
+	 * 	(i) max_end_here = Max(arrA[i], max_end_here+a[i]); 
+	 * 	(ii) max_so_far = Max(max_so_far,max_end_here);
+	 * 
+	 * return max_so_far
+	 * 
+	 * @param arrA
+	 * @return
+	 */
+	public int KandaneModify(int[] arrA) {
+		int max_end_here = arrA[0];
+		int max_so_far = arrA[0];
+		for (int i = 1; i < arrA.length; i++) {
+			max_end_here = Math.max(arrA[i], max_end_here + arrA[i]);
+			max_so_far = Math.max(max_so_far, max_end_here);
+		}
+		return max_so_far;
+	}
+
     public static void main(String args[]){
         KadaneWrapArray kwa = new KadaneWrapArray();
         int input[] = {12, -2, -6, 5, 9, -7, 3};
         int input1[] = {8, -8, 9, -9, 10, -11, 12};
-        System.out.println(kwa.kadaneWrap(input));
+		int[] A = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(kwa.kadaneWrap(A));
         System.out.println(kwa.kadaneWrap(input1));
+		System.out.println(kwa.naiveSoln(A));
+		System.out.println(kwa.kandane(A));
     }
 }
